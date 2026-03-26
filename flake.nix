0a82bc2264093564383daef25cb54d97a6e49fcb
Raine Virta <raine.virta@gmail.com>
2026-03-26 16:00:00 +0200

    fix nix build: add outputHashes for crossterm git dependency
    The nix build was failing because flake.nix didn't include outputHashes
    for the git dependency crossterm-0.29.0 (from raine/crossterm fork).
    Nix requires explicit hashes for git dependencies when using cargoLock.
    
    Adds the SRI hash for crossterm-0.29.0 at commit f99eeae4.

--- flake.nix
+++ flake.nix
@@ -14,7 +14,12 @@
           pname = "workmux";
           version = self.shortRev or self.dirtyShortRev or "dev";
           src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "crossterm-0.29.0" = "sha256-rfAaqGylDaxx3bjmofifnzSh7Hmh21BzHp5fS/w2Z6I=";
+            };
+          };
           nativeBuildInputs = [ pkgs.installShellFiles pkgs.git ];
           postInstall = ''
             export HOME=$TMPDIR
